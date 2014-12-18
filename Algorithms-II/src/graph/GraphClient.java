8ea578ef89f8245707c5b85e2a3930c74f3f955f
eelectron <prashantfromindia@gmail.com>
2014-12-18 14:33:49 +0530

    added class for EDGE

--- Algorithms-II/src/graph/GraphClient.java
+++ Algorithms-II/src/graph/GraphClient.java
@@ -1,23 +1,33 @@
 package graph;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.util.Scanner;
 
 public class GraphClient {
 	public static void main(String[] args) {
-		Scanner sc=new Scanner(System.in);
+		Scanner sc = null;
+		try {
+			sc = new Scanner(new FileReader(args[0]));
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
 		int v=sc.nextInt();		//scans no. of vertex
 		
 		//creates graph with v vertices without edges
 		Graph G=new Graph(v);
 		
 		//add edges to graph
-		while(sc.hasNext()){
+		while(sc.hasNextInt()){
 			G.addEdge(sc.nextInt(), sc.nextInt());
 		}
 		
-		//print graph
-		for (int i = 0; i < G; i++) {
-			
+		//print graphs edge
+		for (int i = 0; i < G.getVertexCount(); i++) {
+			for(int w: G.adj(i)){
+				System.out.println(i+"--"+w);
+			}
 		}
 	}
 }
